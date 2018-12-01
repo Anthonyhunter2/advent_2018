2f802bb95f9f00f15cc9fb4fe6207e6d40324b9b
Anthony <anthony.mazzatenta@atsgroup.com>
2018-12-01 02:27:02 -0500

    Inital commit

---
+++ day1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func readInputToList() []int {
+	var workingList []int
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Print("Counldn't read file")
+	}
+	defer file.Close()
+	reader := bufio.NewScanner(file)
+	for reader.Scan() {
+		line := reader.Text()
+		lineNumber, _ := strconv.Atoi(line)
+		workingList = append(workingList, lineNumber)
+	}
+	return workingList
+}
+func checkForDups(newNum int, alreadyHave []int) bool {
+	// fmt.Printf("Already have = %v\n", alreadyHave)
+	for _, values := range alreadyHave {
+		if newNum == values {
+			fmt.Printf("Found %v in alreadyhave\n", newNum)
+			return true
+		}
+	}
+	return false
+}
+func main() {
+	workingList := readInputToList()
+	counter := 0
+	interations := 1
+	var alreadyseen []int
+	for index := 0; index <= len(workingList); index++ {
+		if index == len(workingList) {
+			index = 0
+		}
+		newNum := workingList[index]
+		counter = counter + newNum
+		mycheck := checkForDups(counter, alreadyseen)
+		if !mycheck {
+			//			counter = counter + newNum
+			alreadyseen = append(alreadyseen, counter)
+			interations = interations + 1
+		} else {
+			index = len(workingList) + 2
+		}
+		// break
+	}
+	fmt.Printf("%v\n", interations)
+}
